dcb448aa9b3c4d56529470451f266601d1a2bb2d
Daniel Fernandes <daniel.f2005@aluno.ifsc.edu.br>
2023-08-09 16:25:16 -0300

    Implementando menu iniciar do sistema

--- lista-01/main.c
+++ lista-01/main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+
+    int digito;
+
+    printf("Bem-vindo ao nosso sistema!\n\n");
+    printf("1- Maior numero.\n2- Sorteador de numeros."
+           "\n3- Quantidade de numeros pares e impares.\n4- Jogo de azar.\n"
+           "5- Numero excluido do vetor.\n6- Matriz identidade.\n"
+           "7- Palindromo.\n8- Contador de vogais.\n\n");
+    printf("Entre com o numero do exercicio: ");
+    scanf("%d", &digito);
+
+    switch (digito) {
+        case 1: printf("Nao implementado!\n"); break;
+        case 2: printf("Nao implementado!\n"); break;
+        case 3: printf("Nao implementado!\n"); break;
+        case 4: printf("Nao implementado!\n"); break;
+        case 5: printf("Nao implementado!\n"); break;
+        case 6: printf("Nao implementado!\n"); break;
+        case 7: printf("Nao implementado!\n"); break;
+        case 8: printf("Nao implementado!\n"); break;
+    }
+
     return 0;
 }
