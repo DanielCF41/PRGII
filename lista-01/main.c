e393d7588bb87ecf1d0f5270d87ce776c5faa568
Daniel Fernandes <daniel.f2005@aluno.ifsc.edu.br>
2023-08-11 17:32:24 -0300

    Ajustando layout do menu

--- lista-01/main.c
+++ lista-01/main.c
@@ -12,23 +12,27 @@ int main(int argc, char *argv[]) {
 
     int digito;
 
-    printf("Bem-vindo ao nosso sistema!\n\nEscolha um exercicio:\n\n");
+    printf("Bem-vindo ao nosso sistema!\n\nEscolha um exercicio:\n");
+    printf("====================================================\n\n");
     printf("1- Maior numero.\n2- Sorteador de numeros."
            "\n3- Quantidade de numeros pares e impares.\n4- Jogo de azar.\n"
            "5- Numero excluido do vetor.\n6- Matriz identidade.\n"
            "7- Palindromo.\n8- Contador de vogais.\n\n");
+
+    printf("====================================================\n");
     printf("Entre com o numero do exercicio:");
     scanf("%d", &digito);
+    printf("====================================================\n");
 
     switch (digito) {
-        case 1: encontra_maior(); break;
-        case 2: sorteio(); break;
-        case 3: conta_par_e_impar(); break;
-        case 4: jogo_de_azar(); break;
-        case 5: vetor(); break;
-        case 6: matriz(); break;
-        case 7: palindromo(); break;
-        case 8: conta_vogal(); break;
+        case 1: printf("EXERCICIO 01\n\n");encontra_maior(); break;
+        case 2: printf("EXERCICIO 02\n\n");sorteio(); break;
+        case 3: printf("EXERCICIO 03\n\n");conta_par_e_impar(); break;
+        case 4: printf("EXERCICIO 04\n\n");jogo_de_azar(); break;
+        case 5: printf("EXERCICIO 05\n\n");vetor(); break;
+        case 6: printf("EXERCICIO 06\n\n");matriz(); break;
+        case 7: printf("EXERCICIO 07\n\n");palindromo(); break;
+        case 8: printf("EXERCICIO 08\n\n");conta_vogal(); break;
     }
 
     return 0;
